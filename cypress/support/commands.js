a0761715df8beb91fd2b4ba51dd50dd956d0cbc5
leonardoSzarblewski <leonardo27.laurindo@gmail.com>
2025-12-26 00:24:25 -0300

    Starting project

---
+++ cypress/support/commands.js
@@ -0,0 +1,6 @@
+Cypress.Commands.add('login', (email, password) => {
+    cy.get('#email').type(email)
+    cy.get('#password').type(password)
+        
+    cy.contains('button', 'Entrar').click()
+})
