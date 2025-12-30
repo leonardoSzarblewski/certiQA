ecbb17cfc79bf655ba713d741708e0373a889ae2
leonardoSzarblewski <leonardo27.laurindo@gmail.com>
2025-12-30 20:47:02 -0300

    implement custom command to fill signup form

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,19 @@
+import { faker } from '@faker-js/faker';
+
 Cypress.Commands.add('login', (email, password) => {
     cy.get('#email').type(email)
     cy.get('#password').type(password)
         
     cy.contains('button', 'Entrar').click()
 })
+
+Cypress.Commands.add('fillSignupForm', () => {
+   const password = faker.string.numeric(6)
+
+    cy.get('#name').type(faker.person.fullName())
+    cy.get('#email').type(faker.internet.email())
+    cy.get('#password').type(password)
+    cy.get('#confirmPassword').type(password)
+
+})
+
