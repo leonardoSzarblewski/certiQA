0d75fafcfbfbb989ecf5c8edd873cf39870649b9
leonardoSzarblewski <leonardo27.laurindo@gmail.com>
2026-01-01 17:27:14 -0300

    add run mode and error screenshots to the pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on, config) {},
+
+    retries: {
+      runMode: 1, // Quantidade de tentativas extras para rodar os testes que falharam na pipeline
     },
 
     baseUrl: 'https://certiqa-qazando.com/',
-
   },
-});
+})
