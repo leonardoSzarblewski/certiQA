ce5feff50dcab456ec7aba39956415d69a11de72
leonardoSzarblewski <leonardo27.laurindo@gmail.com>
2026-01-01 17:37:12 -0300

    add videos and error screenshots to the pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,8 @@ module.exports = defineConfig({
       runMode: 1, // Quantidade de tentativas extras para rodar os testes que falharam na pipeline
     },
 
+    video: true,
+    screenshotOnRunFailure: true,
     baseUrl: 'https://certiqa-qazando.com/',
   },
 })
